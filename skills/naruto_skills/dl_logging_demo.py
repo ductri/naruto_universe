7c1ab1ab6b785bed997df6073a3fc2dfca38cd71
ductri <ductricse@gmail.com>
2019-06-06 15:35:47 +0700

    OK

--- skills/naruto_skills/dl_logging_demo.py
+++ skills/naruto_skills/dl_logging_demo.py
@@ -12,14 +12,30 @@ def loss():
 loss_gen = loss()
 
 if __name__ == '__main__':
+
     logging.basicConfig(level=logging.INFO)
     my_logger = DLLogger()
-    my_logger.add_handler(DLTBHandler('tmp/demo_logging'))
+    my_logger.add_handler(DLTBHandler('tmp/exp_1'))
     my_logger.add_handler(DLLoggingHandler())
     interval = 100
 
     for step in range(10000):
         if step % interval == 0:
             my_logger.add_scalar('Loss/loss1', next(loss_gen), step)
+            my_logger.add_scalar('Loss/loss2', next(loss_gen), step)
+            my_logger.add_scalar('Loss/loss3', next(loss_gen), step)
+
+    my_logger.close()
+
+    my_logger = DLLogger()
+    my_logger.add_handler(DLTBHandler('tmp/exp_2'))
+    my_logger.add_handler(DLLoggingHandler())
+    interval = 100
+
+    for step in range(10000):
+        if step % interval == 0:
+            my_logger.add_scalar('Loss/loss1', next(loss_gen), step)
+            my_logger.add_scalar('Loss/loss2', next(loss_gen), step)
+            my_logger.add_scalar('Loss/loss3', next(loss_gen), step)
 
     my_logger.close()

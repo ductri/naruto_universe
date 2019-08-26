cde0a415ae1777e45a09a336044262aafaeb0de1
ductri <ductricse@gmail.com>
2019-08-26 20:00:27 +0700

    remove some old stuff

---
+++ skills/naruto_skills/text.py
@@ -0,0 +1,15 @@
+import re
+import unicodedata
+
+
+def remove_vietnamese_accent(utf8_str):
+    utf8_str = unicodedata.normalize('NFKC', utf8_str)
+    INTAB = "ạảãàáâậầấẩẫăắằặẳẵóòọõỏôộổỗồốơờớợởỡéèẻẹẽêếềệểễúùụủũưựữửừứíìịỉĩýỳỷỵỹđẠẢÃÀÁÂẬẦẤẨẪĂẮẰẶẲẴÓÒỌÕỎÔỘỔỖỒỐƠỜỚỢỞỠÉÈẺẸẼÊẾỀỆỂỄÚÙỤỦŨƯỰỮỬỪỨÍÌỊỈĨÝỲỶỴỸĐ"
+
+    OUTTAB = "a" * 17 + "o" * 17 + "e" * 11 + "u" * 11 + "i" * 5 + "y" * 5 + "d" + \
+             "A" * 17 + "O" * 17 + "E" * 11 + "U" * 11 + "I" * 5 + "Y" * 5 + "D"
+
+    r = re.compile("|".join(INTAB))
+    replaces_dict = dict(zip(INTAB, OUTTAB))
+
+    return r.sub(lambda m: replaces_dict[m.group(0)], utf8_str)
